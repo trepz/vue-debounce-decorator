dfd166c95753c6ad211eebb50be9e7ebac965026
Robert Worman <tr3ppy@gmail.com>
2019-04-02 08:52:02 +1000

    Add component factory for tests

--- tests/unit/debounce.spec.ts
+++ tests/unit/debounce.spec.ts
@@ -3,5 +3,23 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import flushPromises from 'flush-promises'
 import { mount } from '@vue/test-utils'
+import { Debounce } from '../../src/vue-debounce-decorator'
 
-describe('init', () => {})
+// Component factory
+const factory = (debounceTime: number = 500, componentOptions?: any) => {
+  @Component({
+    template: `
+      <div>{{ someValue }}</div>
+    `,
+    ...componentOptions,
+  })
+  class Comp extends Vue {
+    someValue = 'Hello'
+
+    @Debounce(debounceTime)
+    bounced() {}
+  }
+  return Comp
+}
+
+test('basic debouncing', () => {})
