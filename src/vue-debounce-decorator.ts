631a008478f53324cebacdb8a112d8b7ddd0c0f1
Robert Worman <tr3ppy@gmail.com>
2019-04-03 09:37:04 +1000

    Debounce function with some loose typing

--- src/vue-debounce-decorator.ts
+++ src/vue-debounce-decorator.ts
@@ -1,5 +1,32 @@
 import { createDecorator } from 'vue-class-component'
 
 export function Debounce(time: number): PropertyDecorator {
-  return createDecorator(() => {})
+  return createDecorator((opts, handler) => {
+    if (!opts.methods) throw new Error('This decorator must be used on a vue instance.')
+    opts.methods[handler] = debounce(opts.methods[handler], time, this)
+  })
+}
+
+export function debounce(fun: any, time: number, thisArg: any) {
+  let timeoutId: number | undefined
+
+  const wrapper = Object.assign(
+    (...args: any[]) => {
+      wrapper.clear()
+
+      timeoutId = setTimeout(() => {
+        timeoutId = undefined
+        fun.apply(thisArg, args)
+      }, time)
+    },
+    {
+      clear() {
+        if (timeoutId) {
+          clearTimeout(timeoutId)
+          timeoutId = undefined
+        }
+      },
+    },
+  )
+  return wrapper
 }
