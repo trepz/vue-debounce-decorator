6ac09ef3272a588e79e3df0e24587da35fb095b2
Robert Worman <tr3ppy@gmail.com>
2019-04-03 10:15:12 +1000

    Change return type to method decorator

--- src/vue-debounce-decorator.ts
+++ src/vue-debounce-decorator.ts
@@ -1,6 +1,6 @@
 import { createDecorator } from 'vue-class-component'
 
-export function Debounce(time: number): PropertyDecorator {
+export function Debounce(time: number): MethodDecorator {
   return createDecorator((opts, handler) => {
     if (!opts.methods) throw new Error('This decorator must be used on a vue instance.')
     opts.methods[handler] = debounce(opts.methods[handler], time, this)
