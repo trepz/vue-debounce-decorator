d64648c2de152193b830265b12dabdfc575d1e65
Robert Worman <tr3ppy@gmail.com>
2019-04-16 11:36:06 +1000

    Refactor create decorator

--- src/vue-debounce-decorator.ts
+++ src/vue-debounce-decorator.ts
@@ -2,8 +2,25 @@ import { createDecorator } from 'vue-class-component'
 
 export function Debounce(time: number): MethodDecorator {
   return createDecorator((opts, handler) => {
-    if (!opts.methods) throw new Error('This decorator must be used on a vue instance.')
-    opts.methods[handler] = debounce(opts.methods[handler], time, this)
+    if (!opts.methods) throw new Error('This decorator must be used on a vue component method.')
+
+    const originalFn = opts.methods[handler]
+    let timeoutId = 0
+
+    const clear = () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+        timeoutId = 0
+      }
+    }
+
+    opts.methods[handler] = function(...args: any[]) {
+      clear()
+      timeoutId = setTimeout(() => {
+        timeoutId = 0
+        originalFn.apply(this, args)
+      }, time)
+    }
   })
 }
 
