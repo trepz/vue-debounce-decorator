e3a9cba929130c8f435702d8d8e7008976f5af92
Robert Worman <tr3ppy@gmail.com>
2019-04-20 17:59:36 +1000

    Accept a number or options object

--- src/vue-debounce-decorator.ts
+++ src/vue-debounce-decorator.ts
@@ -1,8 +1,16 @@
 import { createDecorator } from 'vue-class-component'
 
-export function Debounce(time: number): MethodDecorator {
+export type DebounceOptions =
+  | number
+  | {
+      time: number
+    }
+
+export function Debounce(options: DebounceOptions): MethodDecorator {
   return createDecorator((opts, handler) => {
     if (!opts.methods) throw new Error('This decorator must be used on a vue component method.')
+
+    const time = typeof options === 'number' ? options : options.time
 
     const originalFn = opts.methods[handler]
     let timeoutId = 0
