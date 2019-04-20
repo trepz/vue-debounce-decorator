48c9d7d18d1e6737a80a71a26853770b8931eb68
Robert Worman <tr3ppy@gmail.com>
2019-04-20 17:38:13 +1000

    Create basic example component

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="button" @click="increment">Clicked {{ timesClicked }} times!</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import { Debounce } from '../../src/vue-debounce-decorator'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  timesClicked = 0
+
+  @Debounce(500)
+  increment() {
+    this.timesClicked++
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button {
+  background: orange;
+  max-width: 400px;
+  border-radius: 4px;
+  padding: 10px;
+  color: white;
+  margin: 30px auto;
+  cursor: pointer;
+  user-select: none;
+  font-size: 30px;
 }
 </style>
